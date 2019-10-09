214479444e7e2b1497453c387bc5769fb13cf58c
Lucas Taves <lucas.taves@devfactory.com>
2019-10-09 08:03:31 -0300

    CO-8295 Autoscaling analyzer now receives all credential data

--- autoScalingAnalyzer.py
+++ autoScalingAnalyzer.py
@@ -5,33 +5,37 @@ class AutoScalingAnalyzer:
     '''Routine to Analyze IOP Provisioned volumes and calculate Gp2 Costs'''
 
     def __init__(self, **kwargs):
-        ec2 = boto3.client('ec2')
-        self.regions = [region['RegionName']
-                        for region in ec2.describe_regions()['Regions']]
+        self.region = kwargs['Region']
+        self.accountId = kwargs['AccountId']
+        self.accessKeyId = kwargs['AccessKeyId']
+        self.secretKey = kwargs['SecretAccessKey']
+        self.session = kwargs['SessionToken']
 
     def run(self):
         emptyGroups = []
 
-        for region in self.regions:
-            client = boto3.client('autoscaling', region_name=region)
-            autoScaleReponse = client.describe_auto_scaling_groups()
+        client = boto3.client('autoscaling', region_name=self.region,
+                           aws_access_key_id=self.accessKeyId,
+                           aws_secret_access_key=self.secretKey,
+                           aws_session_token=self.session)
+        autoScaleReponse = client.describe_auto_scaling_groups()
 
-            while True:
-                groups = autoScaleReponse['AutoScalingGroups']
+        while True:
+            groups = autoScaleReponse['AutoScalingGroups']
 
-                for group in groups:
-                    loadBalancers = client.describe_load_balancers(
-                        group['AutoScalingGroupName'])['LoadBalancers']
-                    if len(group['Instances']) == 0 and len(loadBalancers) == 0:
-                        emptyGroups.append({
-                            'AutoScalingGroupARN': group['AutoScalingGroupARN'],
-                            'AutoScalingGroupName': group['AutoScalingGroupName'],
-                            'AvailabilityZones': group['AvailabilityZones']})
+            for group in groups:
+                loadBalancers = client.describe_load_balancers(
+                    group['AutoScalingGroupName'])['LoadBalancers']
+                if len(group['Instances']) == 0 and len(loadBalancers) == 0:
+                    emptyGroups.append({
+                        'AutoScalingGroupARN': group['AutoScalingGroupARN'],
+                        'AutoScalingGroupName': group['AutoScalingGroupName'],
+                        'AvailabilityZones': group['AvailabilityZones']})
 
-                if 'NextToken' in autoScaleReponse:
-                    autoScaleReponse = client.describe_auto_scaling_groups(
-                        NextToken=autoScaleReponse['NextToken'])
-                else:
-                    break
+            if 'NextToken' in autoScaleReponse:
+                autoScaleReponse = client.describe_auto_scaling_groups(
+                    NextToken=autoScaleReponse['NextToken'])
+            else:
+                break
 
         return emptyGroups
